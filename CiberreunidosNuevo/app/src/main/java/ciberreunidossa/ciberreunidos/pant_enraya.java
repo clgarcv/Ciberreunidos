9907ce830513cfc975ca42906999996dec050be4
clgarcv <claragarcia792@gmail.com>
2016-03-15 19:47:54 +0100

    pantallas e imagenes

---
+++ CiberreunidosNuevo/app/src/main/java/ciberreunidossa/ciberreunidos/pant_enraya.java
@@ -0,0 +1,13 @@
+package ciberreunidossa.ciberreunidos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class pant_enraya extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pant_enraya);
+    }
+}
