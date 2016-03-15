c768cee6780e488f969fc74ca354ca28dfd59c7b
santaformo <santa_formo@hotmail.com>
2016-03-15 17:32:39 +0100

    funcionalidad botones ronda

--- CiberreunidosNuevo/app/src/main/java/ciberreunidossa/ciberreunidos/piedrapapeltijera.java
+++ CiberreunidosNuevo/app/src/main/java/ciberreunidossa/ciberreunidos/piedrapapeltijera.java
@@ -1,13 +1,47 @@
 package ciberreunidossa.ciberreunidos;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class piedrapapeltijera extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_piedrapapeltijerainicio);
+
+        Button bt_3= (Button) findViewById(R.id.tres);
+        Button bt_5= (Button) findViewById(R.id.cinco);
+        Button bt_7= (Button) findViewById(R.id.siete);
+
+        bt_3.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View view) {
+                Intent i = new Intent(piedrapapeltijera.this, piedrapapeltijera.class); //cambiar clase
+                i.putExtra("ronda",3);
+                startActivity(i);
+            }
+        });
+        bt_5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(piedrapapeltijera.this, piedrapapeltijera.class); //cambiar clase
+                i.putExtra("ronda",3);
+                startActivity(i);
+            }
+        });
+        bt_7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(piedrapapeltijera.this, piedrapapeltijera.class); //cambiar clase
+                i.putExtra("ronda",3);
+                startActivity(i);
+            }
+        });
+
     }
+
+
 }
