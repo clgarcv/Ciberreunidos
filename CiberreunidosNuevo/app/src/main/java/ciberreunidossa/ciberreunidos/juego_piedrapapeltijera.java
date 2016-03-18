aaa76aad36c87a4c862cecabb0133701bd834f5b
santaformo <santa_formo@hotmail.com>
2016-03-18 17:19:26 +0100

    Solo una actividad de resultado y numero de rondas hasta 13

--- CiberreunidosNuevo/app/src/main/java/ciberreunidossa/ciberreunidos/juego_piedrapapeltijera.java
+++ CiberreunidosNuevo/app/src/main/java/ciberreunidossa/ciberreunidos/juego_piedrapapeltijera.java
@@ -1,8 +1,6 @@
 package ciberreunidossa.ciberreunidos;
 
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,8 +11,9 @@ import logicappt.ppt;
 
 public class juego_piedrapapeltijera extends AppCompatActivity{
     public  ppt ppt= new ppt();
-    public int jugador1;
     public boolean ganaR=false;
+    int j=0;
+    int r=1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -77,7 +76,8 @@ public class juego_piedrapapeltijera extends AppCompatActivity{
             }
         });
     }
-
+    //Esta funcion suma un punto en el marcador indicado.
+    //coge el texto del marcador, lo pasa a entero, le suma uno y pone el resulatdo en el marcador
     public void sumar_punto(TextView marcador){
         marcador.getText();
         char a= marcador.getText().charAt(0);
@@ -86,7 +86,8 @@ public class juego_piedrapapeltijera extends AppCompatActivity{
         char b= Integer.toString(i).charAt(0);
         marcador.setText(new String(new char[]{b}));
     }
-
+    //con esta funcion, ponemos una imagen en la mano del jugador 2, para ello le pasamos un string
+    //que sera sacado con una funcion aleatoria.
     public int jugador2(String s){
         int i;
         ImageButton im2= (ImageButton) findViewById(R.id.imageButton_2);
@@ -101,7 +102,9 @@ public class juego_piedrapapeltijera extends AppCompatActivity{
             i=3;}
         return i;
     }
-
+    // Esta función lo que hace es comparar las manos de los 2 jugadores y poner
+    // al marcador el punto del ganador de esa ronda.Tambien dice si se ha ganado
+    // una ronda o a habido un empate, en ese caso no gana nadie la ronda.
     public void juego(int valorJug1,int valorJug2){
         // papel - tijera o tijera - piedra o piedra - papel, gana el segundo
         if(valorJug1 == 1 && valorJug2 == 2 || valorJug1 == 2 && valorJug2 == 3 || valorJug1 == 3 && valorJug2 == 1 ){
@@ -118,25 +121,43 @@ public class juego_piedrapapeltijera extends AppCompatActivity{
         else{ganaR=false;}
 
     }
+    // Esta función cambia el numero de ronda en el texto
     public void cambiar_ronda(TextView marcador){
         marcador.getText();
         char a= marcador.getText().charAt(6);
         int i= Character.digit(a, 10);
         i++;
-        char b= Integer.toString(i).charAt(0);
-        marcador.setText(new String(new char[]{'R','O','N','D','A',' ',b}));
-    }
+        r++;
+        // si hay mas de 9 rondas jugadas, poner RONDA 1x
+        // puesto que no va a haber mas de 13 rondas, puesto que cuando un jugador
+        // llega a 7(maximo de puntos que podemos jugar) termina la partida
+        if (r>9){
+            char b= Integer.toString(i).charAt(0);
+            char c= Integer.toString(j).charAt(0);
+            marcador.setText(new String(new char[]{'R','O','N','D','A',' ','1',c}));
+            j++;
+        }
+        else{
+            char b= Integer.toString(i).charAt(0);
+            marcador.setText(new String(new char[]{'R','O','N','D','A',' ',b}));
+        }
 
+    }
+    // Esta funcion comprueba los marcadores y si es igual al maximo de puntos
+    // entonces salta a las actividades de ganar o perder en funcion de quien
+    // de los 2 jugadores gana
     public void terminarPartida(TextView m1,TextView m2,String rondas){
         m1.getText();
         m2.getText();
         if(m1.getText().toString().equals(rondas) || m2.getText().toString().equals(rondas) ){
             if(m1.getText().toString().equals(rondas)){
-                Intent i = new Intent(juego_piedrapapeltijera.this,  gana_ppt.class);
+                Intent i = new Intent(juego_piedrapapeltijera.this,  pierdegana.class);
+                i.putExtra("resultado","gana");
                 startActivity(i);
             }
             else{
-                Intent i = new Intent(juego_piedrapapeltijera.this, pierde_ppt.class);
+                Intent i = new Intent(juego_piedrapapeltijera.this, pierdegana.class);
+                i.putExtra("resultado","pierde");
                 startActivity(i);
             }
         }
--- CiberreunidosNuevo/app/src/main/java/ciberreunidossa/ciberreunidos/pierdegana.java
+++ CiberreunidosNuevo/app/src/main/java/ciberreunidossa/ciberreunidos/pierdegana.java
@@ -5,29 +5,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-import java.sql.BatchUpdateException;
-
-public class pierde_ppt extends AppCompatActivity {
+public class pierdegana extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pierde_ppt);
+        setContentView(R.layout.activity_pierdegana);
         Button jugar= (Button) findViewById(R.id.jugar);
         Button menu= (Button) findViewById(R.id.menu);
-
+        Bundle bundle = getIntent().getExtras();
+        final String resultado= bundle.getString("resultado");
+        TextView texto=(TextView) findViewById(R.id.resultado);
+        if( resultado.equals("gana")) {
+            texto.setText("HAS GANADO");
+        }
         jugar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(pierde_ppt.this,  piedrapapeltijera.class);
+                Intent i = new Intent(pierdegana.this, piedrapapeltijera.class);
                 startActivity(i);
             }
         });
+
         menu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(pierde_ppt.this,  pant_ppal.class);
+                Intent i = new Intent(pierdegana.this,  pant_ppal.class);
                 startActivity(i);
             }
         });
