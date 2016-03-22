9d4bdcdebdc0180260cdc01f83997a48d378d8d7
daguzman93 <danielagustinas@gmail.com>
2016-03-22 14:11:06 +0100

    Mejora del boton atras en la principal

--- CiberreunidosNuevo/app/src/main/java/ciberreunidossa/ciberreunidos/pant_ppal.java
+++ CiberreunidosNuevo/app/src/main/java/ciberreunidossa/ciberreunidos/pant_ppal.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ImageButton;
 
@@ -48,8 +49,16 @@ public class pant_ppal extends AppCompatActivity {
 
 
 
-
-
-
+    }
+    //Boton atras para salir de la aplicacion
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_HOME);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        }
+        return true;
     }
 }
