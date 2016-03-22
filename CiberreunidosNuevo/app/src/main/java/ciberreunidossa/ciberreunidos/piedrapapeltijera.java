334cea850dc36a294b02401db6d5f482e8e0a763
daguzman93 <danielagustinas@gmail.com>
2016-03-22 16:43:05 +0100

    Ultimas modifiaciones sprint 2

--- CiberreunidosNuevo/app/src/main/java/ciberreunidossa/ciberreunidos/piedrapapeltijera.java
+++ CiberreunidosNuevo/app/src/main/java/ciberreunidossa/ciberreunidos/piedrapapeltijera.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 
@@ -43,6 +44,14 @@ public class piedrapapeltijera extends AppCompatActivity {
                 startActivity(i);
             }
         });
+    }
+    //Cambio comportamiento boton atras
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            this.startActivity(new Intent(piedrapapeltijera.this, pant_ppal.class));
+        }
+        return true;
     }
 
 
