37ed3b80069caa4c772151985df9a37716e93d50
daguzman93 <danielagustinas@gmail.com>
2016-04-06 12:09:02 +0200

    Intro ahorcado

--- CiberreunidosNuevo/app/src/main/java/ciberreunidossa/ciberreunidos/pant_ahorcado.java
+++ CiberreunidosNuevo/app/src/main/java/ciberreunidossa/ciberreunidos/pant_ahorcado.java
@@ -1,19 +1,140 @@
 package ciberreunidossa.ciberreunidos;
 
 import android.app.ActionBar;
+import android.inputmethodservice.Keyboard;
+import android.inputmethodservice.KeyboardView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.EditText;
+
+import java.util.Random;
 
 public class pant_ahorcado extends AppCompatActivity {
+
+    String resultado = "";
+    String palabra;
+
+    String[] palabrasn1 = {"A Coruña", "Álava", "Albacete", "Alicante", "Almería", "Araba", "Asturias", "Ávila",
+            "Badajoz", "Baleares", "Barcelona", "Bizkaia", "Burgos", "Cáceres", "Cádiz", "Cantabria", "Castellón",
+            "Ceuta", "Ciudad Real", "Córdoba", "Cuenca", "Gerona", "Gipuzkoa", "Girona", "Granada", "Guadalajara",
+            "Huelva", "Huesca", "Illes Balears", "Jaén", "La Rioja", "Las Palmas", "León", "Lérida", "Lleida", "Lugo",
+            "Madrid", "Málaga", "Melilla", "Murcia", "Navarra", "Orense", "Ourense", "Palencia", "Pontevedra",
+            "Salamanca", "Santa Cruz de Tenerife", "Segovia", "Sevilla", "Soria", "Tarragona", "Teruel", "Toledo",
+            "Valencia", "Valladolid", "Vizcaya", "Zamora", "Zaragoza"};
+
+    String[] palabrasn2 = {"A Coruña", "Álava", "Albacete", "Alicante", "Almería", "Araba", "Asturias", "Ávila",
+            "Badajoz", "Baleares", "Barcelona", "Bizkaia", "Burgos", "Cáceres", "Cádiz", "Cantabria", "Castellón",
+            "Ceuta", "Ciudad Real", "Córdoba", "Cuenca", "Gerona", "Gipuzkoa", "Girona", "Granada", "Guadalajara",
+            "Huelva", "Huesca", "Illes Balears", "Jaén", "La Rioja", "Las Palmas", "León", "Lérida", "Lleida", "Lugo",
+            "Madrid", "Málaga", "Melilla", "Murcia", "Navarra", "Orense", "Ourense", "Palencia", "Pontevedra",
+            "Salamanca", "Santa Cruz de Tenerife", "Segovia", "Sevilla", "Soria", "Tarragona", "Teruel", "Toledo",
+            "Valencia", "Valladolid", "Vizcaya", "Zamora", "Zaragoza"};
+
+    String[] palabrasn3 = {"A Coruña", "Álava", "Albacete", "Alicante", "Almería", "Araba", "Asturias", "Ávila",
+            "Badajoz", "Baleares", "Barcelona", "Bizkaia", "Burgos", "Cáceres", "Cádiz", "Cantabria", "Castellón",
+            "Ceuta", "Ciudad Real", "Córdoba", "Cuenca", "Gerona", "Gipuzkoa", "Girona", "Granada", "Guadalajara",
+            "Huelva", "Huesca", "Illes Balears", "Jaén", "La Rioja", "Las Palmas", "León", "Lérida", "Lleida", "Lugo",
+            "Madrid", "Málaga", "Melilla", "Murcia", "Navarra", "Orense", "Ourense", "Palencia", "Pontevedra",
+            "Salamanca", "Santa Cruz de Tenerife", "Segovia", "Sevilla", "Soria", "Tarragona", "Teruel", "Toledo",
+            "Valencia", "Valladolid", "Vizcaya", "Zamora", "Zaragoza"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pant_ahorcado);
         //getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+        final EditText editText = (EditText) findViewById(R.id.editText);
+        editText.requestFocus();
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+        eligePalabra();
+        creaPalabra(palabra);
+
+
     }
+
+    public void eligePalabra() {
+        Random r = new Random();
+        palabra = palabrasn1[r.nextInt(palabrasn1.length)];
+    }
+
+    public void creaPalabra(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            resultado = resultado + "_ ";
+        }
+        EditText texto = (EditText) findViewById(R.id.editText);
+        texto.setText(resultado);
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        //Keyboard teclado
+        EditText texto = (EditText) findViewById(R.id.editText);
+
+        switch (keyCode) {
+
+            case KeyEvent.KEYCODE_A:
+                texto.setText("aaaa");
+                break;
+            case KeyEvent.KEYCODE_B:
+                break;
+            case KeyEvent.KEYCODE_C:
+                break;
+            case KeyEvent.KEYCODE_D:
+                break;
+            case KeyEvent.KEYCODE_E:
+                break;
+            case KeyEvent.KEYCODE_F:
+                break;
+            case KeyEvent.KEYCODE_G:
+                break;
+            case KeyEvent.KEYCODE_H:
+                break;
+            case KeyEvent.KEYCODE_I:
+                break;
+            case KeyEvent.KEYCODE_J:
+                break;
+            case KeyEvent.KEYCODE_K:
+                break;
+            case KeyEvent.KEYCODE_L:
+                break;
+            case KeyEvent.KEYCODE_M:
+                break;
+            case KeyEvent.KEYCODE_N:
+                break;
+            case KeyEvent.KEYCODE_O:
+                break;
+            case KeyEvent.KEYCODE_P:
+                break;
+            case KeyEvent.KEYCODE_Q:
+                break;
+            case KeyEvent.KEYCODE_R:
+                break;
+            case KeyEvent.KEYCODE_S:
+                break;
+            case KeyEvent.KEYCODE_T:
+                break;
+            case KeyEvent.KEYCODE_U:
+                break;
+            case KeyEvent.KEYCODE_V:
+                break;
+            case KeyEvent.KEYCODE_X:
+                break;
+            case KeyEvent.KEYCODE_Y:
+                break;
+            case KeyEvent.KEYCODE_Z:
+                break;
+            default:
+                return super.onKeyUp(keyCode, event);
+        }
+        return  true;
+
+
+    }
+
+
 }
 
 /*
@@ -37,21 +158,8 @@ public class pant_ahorcado extends AppCompatActivity {
 		return result;
 	}
 
-	public static String ponLetra(String pal, String l) {
-		String result = "";
 
-		String[] separada = pal.split("");
-		// for (String s : separada)
-		// System.out.print(s);
 
-		for (String s : separada) {
-			if (s.compareTo(l) == 0) {
-				result = result + l +" ";
-			} else
-				result = result + "_ ";
-		}
-		return result;
-	}
 
 *
 * */
