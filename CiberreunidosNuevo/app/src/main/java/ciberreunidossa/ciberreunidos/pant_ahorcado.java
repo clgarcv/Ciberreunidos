b6664926900eaf40bf42c193d878df1139c87c29
clgarcv <claragarcia792@gmail.com>
2016-04-07 12:51:41 +0200

    ahorcado v3 a falta de depurar fallos o mejorar funcionamiento

--- CiberreunidosNuevo/app/src/main/java/ciberreunidossa/ciberreunidos/pant_ahorcado.java
+++ CiberreunidosNuevo/app/src/main/java/ciberreunidossa/ciberreunidos/pant_ahorcado.java
@@ -1,12 +1,10 @@
 package ciberreunidossa.ciberreunidos;
 
-import android.inputmethodservice.KeyboardView;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
-import android.view.View;
 import android.view.WindowManager;
-import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -15,56 +13,29 @@ import java.util.Random;
 public class pant_ahorcado extends AppCompatActivity {
 
     String resultado = "";
-    String palabra;
+    String palabra="";
+    int fallos = 0;
 
-    String[] palabrasn1 = {"A Coruña", "Álava", "Albacete", "Alicante", "Almería", "Araba", "Asturias", "Ávila",
-            "Badajoz", "Baleares", "Barcelona", "Bizkaia", "Burgos", "Cáceres", "Cádiz", "Cantabria", "Castellón",
-            "Ceuta", "Ciudad Real", "Córdoba", "Cuenca", "Gerona", "Gipuzkoa", "Girona", "Granada", "Guadalajara",
-            "Huelva", "Huesca", "Illes Balears", "Jaén", "La Rioja", "Las Palmas", "León", "Lérida", "Lleida", "Lugo",
-            "Madrid", "Málaga", "Melilla", "Murcia", "Navarra", "Orense", "Ourense", "Palencia", "Pontevedra",
-            "Salamanca", "Santa Cruz de Tenerife", "Segovia", "Sevilla", "Soria", "Tarragona", "Teruel", "Toledo",
-            "Valencia", "Valladolid", "Vizcaya", "Zamora", "Zaragoza"};
+    String[] palabrasn1 = {"pera","mesa","casa","reloj","carta","silla","cama","lapiz","raton","movil","sofa","cartel","letra","boton","papel","marco"};
 
-    String[] palabrasn2 = {"A Coruña", "Álava", "Albacete", "Alicante", "Almería", "Araba", "Asturias", "Ávila",
-            "Badajoz", "Baleares", "Barcelona", "Bizkaia", "Burgos", "Cáceres", "Cádiz", "Cantabria", "Castellón",
-            "Ceuta", "Ciudad Real", "Córdoba", "Cuenca", "Gerona", "Gipuzkoa", "Girona", "Granada", "Guadalajara",
-            "Huelva", "Huesca", "Illes Balears", "Jaén", "La Rioja", "Las Palmas", "León", "Lérida", "Lleida", "Lugo",
-            "Madrid", "Málaga", "Melilla", "Murcia", "Navarra", "Orense", "Ourense", "Palencia", "Pontevedra",
-            "Salamanca", "Santa Cruz de Tenerife", "Segovia", "Sevilla", "Soria", "Tarragona", "Teruel", "Toledo",
-            "Valencia", "Valladolid", "Vizcaya", "Zamora", "Zaragoza"};
+    String[] palabrasn2 = {"pera","mesa","casa","reloj","carta","silla","cama","lapiz","raton","movil","sofa","cartel","letra","boton","papel","marco"};
 
-    String[] palabrasn3 = {"A Coruña", "Álava", "Albacete", "Alicante", "Almería", "Araba", "Asturias", "Ávila",
-            "Badajoz", "Baleares", "Barcelona", "Bizkaia", "Burgos", "Cáceres", "Cádiz", "Cantabria", "Castellón",
-            "Ceuta", "Ciudad Real", "Córdoba", "Cuenca", "Gerona", "Gipuzkoa", "Girona", "Granada", "Guadalajara",
-            "Huelva", "Huesca", "Illes Balears", "Jaén", "La Rioja", "Las Palmas", "León", "Lérida", "Lleida", "Lugo",
-            "Madrid", "Málaga", "Melilla", "Murcia", "Navarra", "Orense", "Ourense", "Palencia", "Pontevedra",
-            "Salamanca", "Santa Cruz de Tenerife", "Segovia", "Sevilla", "Soria", "Tarragona", "Teruel", "Toledo",
-            "Valencia", "Valladolid", "Vizcaya", "Zamora", "Zaragoza"};
+    String[] palabrasn3 = {"pera","mesa","casa","reloj","carta","silla","cama","lapiz","raton","movil","sofa","cartel","letra","boton","papel","marco"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pant_ahorcado);
-        final TextView palabra = (TextView) findViewById(R.id.palabra);
+        //final TextView palabraT = (TextView) findViewById(R.id.palabra);
+        //final ImageView imgAhorcado = (ImageView) findViewById(R.id.img_ahocado);
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
 
-        EditText aux = (EditText) findViewById(R.id.aux);
-        aux.requestFocus();
-        //eligePalabra();
-        //creaPalabra(palabra.getText().toString());
-
-        /*
-        //onClick para boton ayuda piedra papel tijera
-        palabra.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
-            }
-        });
-        */
+        eligePalabra();
+        creaPalabra(palabra);
+        System.out.println(palabra);
 
     }
-/*
+
     public void eligePalabra() {
         Random r = new Random();
         palabra = palabrasn1[r.nextInt(palabrasn1.length)];
@@ -74,110 +45,66 @@ public class pant_ahorcado extends AppCompatActivity {
         for (int i = 0; i < s.length(); i++) {
             resultado = resultado + "_ ";
         }
-        EditText texto = (EditText) findViewById(R.id.palabra);
+        TextView texto = (TextView) findViewById(R.id.palabra);
+        System.out.println(resultado);
         texto.setText(resultado);
+    }
+
+    public String ponLetra(String palabra, String resultado, String letra) {
+
+        String [] palSep = palabra.toUpperCase().split("");
+        String [] resultSep = resultado.toUpperCase().split(" ");
+
+        resultado = "";
+
+        for (int i = 0; i < palSep.length; i++) {
+            //recorro la palabra y compruebo la posicion donde este la letra
+            if(palSep[i].equals(letra)){
+                resultSep[i-1] = letra;
+            }
+        }
+
+        //generamos la palabra a mostrar
+        for (String s : resultSep) {
+            resultado = resultado + s +" ";
+        }
+
+        return resultado;
+    }
+
+    public boolean estaLetra (String palabra, String letra) {
+        return palabra.toUpperCase().contains(letra.toUpperCase());
     }
 
     @Override
     public boolean onKeyUp(int keyCode, KeyEvent event) {
-        //Keyboard teclado
+
+        int img = R.drawable.ahorcado_fallo_+fallos;
+
         TextView texto = (TextView) findViewById(R.id.palabra);
-        EditText aux = (EditText) findViewById(R.id.aux);
-        String l = aux.getText().toString();
-        //texto.setText("");
-        texto.setText(l);
+        ImageView imgAhorcado = (ImageView) findViewById(R.id.img_ahocado);
 
 
+        char l = (char) event.getUnicodeChar();
+        String letra = Character.toString(l).toUpperCase();
+        //texto.setText(Character.toString(letra));
+        System.out.println(letra);
 
-        /*
-        switch (keyCode) {
-
-            case KeyEvent.KEYCODE_A:
-                texto.setText("aaaa");
-                break;
-            case KeyEvent.KEYCODE_B:
-                break;
-            case KeyEvent.KEYCODE_C:
-                break;
-            case KeyEvent.KEYCODE_D:
-                break;
-            case KeyEvent.KEYCODE_E:
-                break;
-            case KeyEvent.KEYCODE_F:
-                break;
-            case KeyEvent.KEYCODE_G:
-                break;
-            case KeyEvent.KEYCODE_H:
-                break;
-            case KeyEvent.KEYCODE_I:
-                break;
-            case KeyEvent.KEYCODE_J:
-                break;
-            case KeyEvent.KEYCODE_K:
-                break;
-            case KeyEvent.KEYCODE_L:
-                break;
-            case KeyEvent.KEYCODE_M:
-                break;
-            case KeyEvent.KEYCODE_N:
-                break;
-            case KeyEvent.KEYCODE_O:
-                break;
-            case KeyEvent.KEYCODE_P:
-                break;
-            case KeyEvent.KEYCODE_Q:
-                break;
-            case KeyEvent.KEYCODE_R:
-                break;
-            case KeyEvent.KEYCODE_S:
-                break;
-            case KeyEvent.KEYCODE_T:
-                break;
-            case KeyEvent.KEYCODE_U:
-                break;
-            case KeyEvent.KEYCODE_V:
-                break;
-            case KeyEvent.KEYCODE_X:
-                break;
-            case KeyEvent.KEYCODE_Y:
-                break;
-            case KeyEvent.KEYCODE_Z:
-                break;
-            default:
-                return super.onKeyUp(keyCode, event);
+        if(estaLetra(palabra, letra)){
+            //si la letra forma parte de la palabra la mostramos
+            resultado = ponLetra(palabra, resultado, letra);
+            texto.setText(resultado);
+        } else {
+            fallos++;
+            //si la letra no forma parte de la palabra actualizamos el ahorcado
+            imgAhorcado.setImageResource(R.drawable.ahorcado_fallo_+fallos);
+            System.out.println(img);
+            System.out.println(R.drawable.ahorcado_fallo_+fallos);
+            System.out.println("la imagen del ahorcado modificada");
         }
+        System.out.println(resultado);
         return true;
 
-
     }
-*/
-
 }
 
-
-/*
-*
-* public class Ahorcado {
-
-	String[] palabras = { "A Coruña", "Álava", "Albacete", "Alicante", "Almería", "Araba", "Asturias", "Ávila",
-			"Badajoz", "Baleares", "Barcelona", "Bizkaia", "Burgos", "Cáceres", "Cádiz", "Cantabria", "Castellón",
-			"Ceuta", "Ciudad Real", "Córdoba", "Cuenca", "Gerona", "Gipuzkoa", "Girona", "Granada", "Guadalajara",
-			"Huelva", "Huesca", "Illes Balears", "Jaén", "La Rioja", "Las Palmas", "León", "Lérida", "Lleida", "Lugo",
-			"Madrid", "Málaga", "Melilla", "Murcia", "Navarra", "Orense", "Ourense", "Palencia", "Pontevedra",
-			"Salamanca", "Santa Cruz de Tenerife", "Segovia", "Sevilla", "Soria", "Tarragona", "Teruel", "Toledo",
-			"Valencia", "Valladolid", "Vizcaya", "Zamora", "Zaragoza" };
-
-	public static String creaPalabra(String s) {
-		String result = "";
-
-		for (int i = 0; i < s.length(); i++) {
-			result = result + "_ ";
-		}
-		return result;
-	}
-
-
-
-
-*
-* */
