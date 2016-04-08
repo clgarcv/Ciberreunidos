f472703ff0cdead6146ed4e7226caa2d7497c330
daguzman93 <danielagustinas@gmail.com>
2016-04-08 19:40:20 +0200

    Boton atras

--- CiberreunidosNuevo/app/src/main/java/ciberreunidossa/ciberreunidos/pant_ahorcado.java
+++ CiberreunidosNuevo/app/src/main/java/ciberreunidossa/ciberreunidos/pant_ahorcado.java
@@ -1,5 +1,6 @@
 package ciberreunidossa.ciberreunidos;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
@@ -13,14 +14,14 @@ import java.util.Random;
 public class pant_ahorcado extends AppCompatActivity {
 
     String resultado = "";
-    String palabra="";
+    String palabra = "";
     int fallos = 0;
 
-    String[] palabrasn1 = {"pera","mesa","casa","reloj","carta","silla","cama","lapiz","raton","movil","sofa","cartel","letra","boton","papel","marco"};
+    String[] palabrasn1 = {"pera", "mesa", "casa", "reloj", "carta", "silla", "cama", "lapiz", "raton", "movil", "sofa", "cartel", "letra", "boton", "papel", "marco"};
 
-    String[] palabrasn2 = {"pera","mesa","casa","reloj","carta","silla","cama","lapiz","raton","movil","sofa","cartel","letra","boton","papel","marco"};
+    String[] palabrasn2 = {"pera", "mesa", "casa", "reloj", "carta", "silla", "cama", "lapiz", "raton", "movil", "sofa", "cartel", "letra", "boton", "papel", "marco"};
 
-    String[] palabrasn3 = {"pera","mesa","casa","reloj","carta","silla","cama","lapiz","raton","movil","sofa","cartel","letra","boton","papel","marco"};
+    String[] palabrasn3 = {"pera", "mesa", "casa", "reloj", "carta", "silla", "cama", "lapiz", "raton", "movil", "sofa", "cartel", "letra", "boton", "papel", "marco"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,55 +53,66 @@ public class pant_ahorcado extends AppCompatActivity {
 
     public String ponLetra(String palabra, String resultado, String letra) {
 
-        String [] palSep = palabra.toUpperCase().split("");
-        String [] resultSep = resultado.toUpperCase().split(" ");
+        String[] palSep = palabra.toUpperCase().split("");
+        String[] resultSep = resultado.toUpperCase().split(" ");
 
         resultado = "";
 
         for (int i = 0; i < palSep.length; i++) {
             //recorro la palabra y compruebo la posicion donde este la letra
-            if(palSep[i].equals(letra)){
-                resultSep[i-1] = letra;
+            if (palSep[i].equals(letra)) {
+                resultSep[i - 1] = letra;
             }
         }
 
         //generamos la palabra a mostrar
         for (String s : resultSep) {
-            resultado = resultado + s +" ";
+            resultado = resultado + s + " ";
         }
 
         return resultado;
     }
 
-    public boolean estaLetra (String palabra, String letra) {
+    public boolean finPartida() {
+
+        if (!resultado.contains("_") || fallos == 7) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public boolean estaLetra(String palabra, String letra) {
         return palabra.toUpperCase().contains(letra.toUpperCase());
     }
 
     @Override
     public boolean onKeyUp(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            this.startActivity(new Intent(pant_ahorcado.this, pant_ppal.class));
+            return true;
+        }
 
-        int img = R.drawable.ahorcado_fallo_+fallos;
+
+        int img = R.drawable.ahorcado_fallo_ + fallos;
 
         TextView texto = (TextView) findViewById(R.id.palabra);
         ImageView imgAhorcado = (ImageView) findViewById(R.id.img_ahocado);
-
-
         char l = (char) event.getUnicodeChar();
         String letra = Character.toString(l).toUpperCase();
         //texto.setText(Character.toString(letra));
         System.out.println(letra);
 
-        if(estaLetra(palabra, letra)){
+        if (estaLetra(palabra, letra)) {
             //si la letra forma parte de la palabra la mostramos
             resultado = ponLetra(palabra, resultado, letra);
             texto.setText(resultado);
         } else {
             fallos++;
             //si la letra no forma parte de la palabra actualizamos el ahorcado
-            imgAhorcado.setImageResource(R.drawable.ahorcado_fallo_+fallos);
-            System.out.println(img);
-            System.out.println(R.drawable.ahorcado_fallo_+fallos);
-            System.out.println("la imagen del ahorcado modificada");
+            imgAhorcado.setImageResource(R.drawable.ahorcado_fallo_ + fallos);
+
         }
         System.out.println(resultado);
         return true;
