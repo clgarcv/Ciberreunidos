45f761f523ea1a5d0b8dcc6e85445fc91e00753c
clgarcv <claragarcia792@gmail.com>
2016-05-17 20:13:28 +0200

    .

---
+++ CiberreunidosNuevo/app/src/main/java/ciberreunidossa/ciberreunidos/MiMusica.java
@@ -0,0 +1,13 @@
+package ciberreunidossa.ciberreunidos;
+
+import android.media.MediaPlayer;
+
+import java.io.Serializable;
+
+/**
+ * Created by Clara on 17/05/2016.
+ */
+public class MiMusica implements Serializable{
+
+    static MediaPlayer melodia = new MediaPlayer();
+}
